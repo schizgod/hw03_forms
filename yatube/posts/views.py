78a68e0ee495c09b2422ab3b46c6740368d185fa
schizgod <schizgod@gmail.com>
2022-10-28 14:56:45 +0300

    gotovo

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
+from django.conf import settings
 
 from core.utils import paginate
 from posts.forms import PostForm
 from posts.models import Group, Post, User
-from yatube.settings import POSTS_NUMBER
+
 
 
 def index(request):
     page_obj = paginate(
-        request, POSTS_NUMBER,
+        request, settings.POSTS_NUMBER,
         Post.objects.select_related('group', 'author').all(),
     )
     return render(
@@ -24,7 +25,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     page_obj = paginate(
-        request, POSTS_NUMBER, group.posts.select_related('author').all(),
+        request, settings.POSTS_NUMBER, group.posts.select_related('author').all(),
     )
     return render(
         request,
@@ -41,7 +42,7 @@ def profile(request, username):
     posts = author.posts.select_related('author', 'group')
     posts_count = posts.count()
     page_obj = paginate(request,
-                        POSTS_NUMBER,
+                        settings.POSTS_NUMBER,
                         author.posts.select_related('author', 'group')
                         )
     context = {
@@ -83,19 +84,6 @@ def post_create(request):
         post.author = request.user
         form.save()
         return redirect('posts:profile', request.user)
-
-
-@login_required
-def post_create(request):
-    form = PostForm(request.POST or None)
-    if form.is_valid():
-        post = form.save(commit=False)
-        post.author = request.user
-        post.save()
-        return redirect('posts:profile', request.user)
-
-    context = {'form': form}
-    return render(request, 'posts/create_post.html', context)
 
 
 @login_required
--- yatube/users/forms.py
+++ yatube/users/forms.py
@@ -1,13 +1,24 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
-
+from django.contrib.auth.forms import UserCreationForm
 
 User = get_user_model()
 
 
 class CreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        # укажем модель, с которой связана создаваемая форма
         model = User
-        # укажем, какие поля должны быть видны в форме и в каком порядке
-        fields = ('first_name', 'last_name', 'username', 'email')
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+        )
+        help_texts = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'username': 'Пользователь',
+            'email': 'Email',
+        }
+        labels = {
+            'username': ('Никнэйм'),
+        }
